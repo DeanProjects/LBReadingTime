3491f5ad61c34f10b1b0c6ebeb18a96bb11f6217
Luca Bernardi <luka.bernardi@gmail.com>
2013-03-12 17:09:53 +0100

    Initial Commit

---
+++ LBReadingTime/LBViewController.m
@@ -0,0 +1,29 @@
+//
+//  LBViewController.m
+//  LBReadingTime
+//
+//  Created by Luca Bernardi on 12/03/13.
+//  Copyright (c) 2013 Luca Bernardi. All rights reserved.
+//
+
+#import "LBViewController.h"
+
+@interface LBViewController ()
+
+@end
+
+@implementation LBViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
